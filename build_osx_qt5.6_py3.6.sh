2d98e847c2ce6003804eaaeafcb87df32e0bdd82
Fredrik Averpil <fredrik@averpil.com>
2017-08-20 17:17:22 +0200

    Add Python 3.6

---
+++ build_osx_qt5.6_py3.6.sh
@@ -0,0 +1,33 @@
+# Always review the official build instructions before updating:
+# https://wiki.qt.io/PySide2_GettingStarted#Building_PySide2
+
+# Homebrew already provided by Travis CI
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Homebrew if not already installed
+if test ! $(which brew)
+then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Xcode already provided by Travis CI
+# xcode-select --install
+# sudo xcodebuild -license
+
+# brew update
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/bccb626274190f00805e60f1e11c8a6edfbe6386/Formula/python3.rb
+brew install https://raw.githubusercontent.com/fredrikaverpil/pyside2-wheels/master/homebrew/qt5/5.6.1-1/qt5.rb
+brew install libxslt libxml2
+
+# OpenSSL already provided by Travis CI
+# brew install openssl
+
+# CMAke 3.0 already provided by Travis CI
+# brew install cmake
+
+git clone --recursive --branch 5.6 https://codereview.qt-project.org/pyside/pyside-setup ~/pyside-setup
+
+# Fix bug https://bugreports.qt.io/browse/PYSIDE-552
+RUN sed -i.bak $'s/if(Qt5Designer_FOUND)/find_package(Qt5Designer)\\\nif(Qt5Designer_FOUND)/g' ~/pyside-setup/sources/pyside2/CMakeLists.txt
+
+python3 ~/pyside-setup/setup.py bdist_wheel --ignore-git --qmake=/usr/local/Cellar/qt5/5.6.1-1/bin/qmake --cmake=/usr/local/bin/cmake --openssl=/usr/local/Cellar/openssl/1.0.2h_1/bin
